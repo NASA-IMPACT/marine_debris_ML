561d5f02e1fdeca2ecdec7a403f4115ffc551d68
Lilly Thomas <lillythomas@Lillys-MacBook-Pro.local>
2021-03-30 18:16:30 -0700

    Adding scripts for uploading images frmo S3 to Image Labeler, and for downloading and processing labeled imagery using label maker

--- data_utils/label_maker_get_training_data_from_S3_IL.py
+++ data_utils/label_maker_get_training_data_from_S3_IL.py
@@ -40,85 +40,6 @@ class Uploader:
         #self.request_token(username, password, client_id, client_secret)
         Uploader.mkdir('updated')
 
-    def read_geotiffs(self, file_name):
-        """
-        Upload geotiffs into imagelabeler
-        Args:
-            file_name (str): path to downloaded geotiff.
-        """
-        foldername, _ = os.path.splitext(file_name)
-        Uploader.mkdir(foldername)
-
-        with ZipFile(file_name) as zip_file:
-            print("================ Reading files ================")
-            compressed_files = zip_file.namelist()
-            for compressed_file in compressed_files:
-                compressed_file = str(compressed_file)
-                _, extension = os.path.splitext(compressed_file)
-                if extension == '.tif':
-                    self.process_geotiff(
-                        compressed_file,
-                        zip_file,
-                        foldername
-                    )
-
-    def process_geotiff(self, compressed_file, zip_file, foldername):
-        """
-        Reproject and upload geotiff into imagelabeler
-        Args:
-            compressed_file (str): path of tif file in zip file
-            zip_file (zipfile.ZipFile): zipfile instance
-            foldername (str): foldername of where to store file
-        """
-        split = compressed_file.split('/')[-1].split('_')
-        updated_filename = f"marine_plastic_{'T'.join(split[0:2])}_{'_'.join(split[2:])}"
-        filename = f"{foldername}/{updated_filename}"
-        mem_tiff = zip_file.read(compressed_file)
-        tiff_file = MemoryFile(mem_tiff).open()
-        updated_profile = self.calculate_updated_profile(tiff_file)
-        with rasterio.open(filename, 'w', **updated_profile) as dst:
-            for band in range(1, 4):
-                reproject(
-                    source=rasterio.band(tiff_file, band),
-                    destination=rasterio.band(dst, band),
-                    src_transform=tiff_file.transform,
-                    src_crs=tiff_file.crs,
-                    dst_transform=updated_profile['transform'],
-                    dst_crs=DEFAULT_CRS,
-                    resampling=Resampling.nearest
-                )
-        #_, status_code = self.upload_to_image_labeler(filename)
-        #if status_code == 200:
-        os.remove(filename)
-        print(f"{filename} processed")
-
-    def calculate_updated_profile(self, tiff_file):
-        """
-        Create updated profile for the provided tiff_file
-        Args:
-            tiff_file (rasterio.io.MemoryFile): rasterio memoryfile.
-        Returns:
-            dict: updated profile for new tiff file
-        """
-        profile = tiff_file.profile
-        transform, width, height = calculate_default_transform(
-            tiff_file.crs,
-            DEFAULT_CRS,
-            tiff_file.width,
-            tiff_file.height,
-            *tiff_file.bounds
-        )
-        profile.update(
-            crs=DEFAULT_CRS,
-            transform=transform,
-            width=width,
-            height=height,
-            count=3,
-            nodata=0,
-            compress='lzw',
-            dtype='uint8'
-        )
-        return profile
 
     def create_training_data(config, new_geo_dics):
         """create lables.npz and tiles from AOIs
@@ -170,6 +91,92 @@ class Uploader:
             coords = np.array(list(geojson.utils.coords(geometry)))
         return [min(xcoords), min(ycoords), max(xcoords), max(ycoords)]
 
+    def read_geotiffs(self, file_name):
+        """
+        Upload geotiffs into imagelabeler
+        Args:
+            file_name (str): path to downloaded geotiff.
+        """
+        foldername, _ = os.path.splitext(file_name)
+        Uploader.mkdir(foldername)
+
+        with ZipFile(file_name) as zip_file:
+            print("================ Reading files ================")
+            compressed_files = zip_file.namelist()
+            for compressed_file in compressed_files:
+                compressed_file = str(compressed_file)
+                _, extension = os.path.splitext(compressed_file)
+                if extension == '.tif':
+                    self.process_geotiff(
+                        compressed_file,
+                        zip_file,
+                        foldername
+                    )
+
+    def process_geotiff(self, compressed_file, zip_file, foldername):
+        """
+        Reproject and upload geotiff into imagelabeler
+        Args:
+            compressed_file (str): path of tif file in zip file
+            zip_file (zipfile.ZipFile): zipfile instance
+            foldername (str): foldername of where to store file
+        """
+        split = compressed_file.split('/')[-1].split('_')
+        updated_filename = f"marine_plastic_{'T'.join(split[0:2])}_{'_'.join(split[2:])}"
+        filename = f"{foldername}/{updated_filename}"
+        mem_tiff = zip_file.read(compressed_file)
+        tiff_file = MemoryFile(mem_tiff).open()
+        updated_profile = self.calculate_updated_profile(tiff_file)
+        with rasterio.open(filename, 'w', **updated_profile) as dst:
+            for band in range(1, 4):
+                reproject(
+                    source=rasterio.band(tiff_file, band),
+                    destination=rasterio.band(dst, band),
+                    src_transform=tiff_file.transform,
+                    src_crs=tiff_file.crs,
+                    dst_transform=updated_profile['transform'],
+                    dst_crs=DEFAULT_CRS,
+                    resampling=Resampling.nearest
+                )
+        #_, status_code = self.upload_to_image_labeler(filename)
+        #if status_code == 200:
+        #os.remove(filename)
+        extent = get_bounding_box([f'{filename}.geojson')
+        config = "config.json"
+        new_geo_dics[f'{filename}.geojson'] = extent
+        create_training_data(config, new_geo_dics)
+        os.remove(filename)
+        print(f"{filename} processed")
+
+    def calculate_updated_profile(self, tiff_file):
+        """
+        Create updated profile for the provided tiff_file
+        Args:
+            tiff_file (rasterio.io.MemoryFile): rasterio memoryfile.
+        Returns:
+            dict: updated profile for new tiff file
+        """
+        profile = tiff_file.profile
+        transform, width, height = calculate_default_transform(
+            tiff_file.crs,
+            DEFAULT_CRS,
+            tiff_file.width,
+            tiff_file.height,
+            *tiff_file.bounds
+        )
+        profile.update(
+            crs=DEFAULT_CRS,
+            transform=transform,
+            width=width,
+            height=height,
+            count=3,
+            nodata=0,
+            compress='lzw',
+            dtype='uint8'
+        )
+        return profile
+
+
     @classmethod
     def mkdir(cls, dirname):
         if not os.path.exists(dirname):
@@ -192,10 +199,11 @@ def main(profile_name, client_id, client_secret):
             print("================ Upload in progress ================")
             uploader.read_geotiffs(zip_filename)
             print("================ Process Complete ================")
-            extent = get_bounding_box([f'{filename}.geojson')
-            new_geo_dics[f'{filename}.geojson'] = extent
-            config = "config.json"
-            create_training_data(config, new_geo_dics)
+            #extent = get_bounding_box([f'{filename}.geojson')
+            #new_geo_dics[f'{filename}.geojson'] = extent
+            #config = "config.json"
+            #create_training_data(config, new_geo_dics)
+            #os.remove(f'{DOWNLOAD_FOLDER}/{filename}.tif')
 
 
 main(profile_name=os.environ['AWS_PROFILE_NAME'], client_id=os.environ['AWS_ACCESS_KEY'], client_secret=os.environ['AWS_SECRET_ACCESS_KEY'])
